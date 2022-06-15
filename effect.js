eae97f7f52cb332e21869d4643f53ba93d59f82b
Rosario Bagaskara <rosariobagaskara@gmail.com>
2022-06-15 19:28:29 +0700

    edited text, change photo's opacity

--- effect.js
+++ effect.js
@@ -291,8 +291,8 @@ $('document').ready(function () {
 
     $('#story').click(function () {
         $(this).fadeOut('slow');
-        var audio = $('.song')[0];
-        audio.pause();
+        // var audio = $('.song')[0];
+        // audio.pause();
         $('.cake').fadeOut('fast').promise().done(function () {
             $('.message').fadeIn('slow');
         });
@@ -303,7 +303,7 @@ $('document').ready(function () {
             $("p:nth-child(" + i + ")").fadeOut('slow').delay(800).promise().done(function () {
                 i = i + 1;
                 $("p:nth-child(" + i + ")").fadeIn('slow').delay(1000);
-                if (i == 50) {
+                if (i == 70) {
                     $("p:nth-child(49)").fadeOut('slow').promise().done(function () {
                         $('.cake').fadeIn('fast');
                     });
